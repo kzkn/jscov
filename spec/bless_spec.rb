74f9b16353e3ea3221f5147e0de730e219a9ec03
Kazuki Nishikawa <kz.nishikawa@gmail.com>
2020-10-10 16:43:45 +0900

    migrate bless_test to rspec

---
+++ spec/bless_spec.rb
@@ -0,0 +1,43 @@
+require "spec_helper"
+
+describe Jscov::Bless do
+  before do
+    Jscov.configuration.reset!
+  end
+
+  it "blesses script" do
+    bless = Jscov::Bless.new([200, {}, ["<html><head></head><body>foo</body></html>"]])
+    result = bless.result
+    expect(result[0]).to eq 200
+    expect(result[1]).to eq({})
+    expect(result[2]).to eq ["<html><head><script>#{Jscov::Bless.js_code}</script></head><body>foo</body></html>"]
+  end
+
+  it "blesses script if head tag separated" do
+    bless = Jscov::Bless.new([200, {}, ["<html><", "h", "e", "a", "d", ">", "</head><body>foo</body></html>"]])
+    result = bless.result
+    expect(result[0]).to eq 200
+    expect(result[1]).to eq({})
+    expect(result[2]).to eq ["<html><head><script>#{Jscov::Bless.js_code}</script>", "</head><body>foo</body></html>"]
+  end
+
+  it "not blesses non html response" do
+    bless = Jscov::Bless.new([200, {}, [%({ "json": true })]])
+    result = bless.result
+    expect(result[0]).to eq 200
+    expect(result[1]).to eq({})
+    expect(result[2]).to eq [%({ "json": true })]
+  end
+
+  it "not bless if jscov disabled" do
+    Jscov.configure do |config|
+      config.enabled = false
+    end
+
+    bless = Jscov::Bless.new([200, {}, ["<html><head></head><body>foo</body></html>"]])
+    result = bless.result
+    expect(result[0]).to eq 200
+    expect(result[1]).to eq({})
+    expect(result[2]).to eq ["<html><head></head><body>foo</body></html>"]
+  end
+end
