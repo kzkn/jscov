fceee8d88f39a90bebe64613570d989fce5037ec
Kazuki Nishikawa <kz.nishikawa@gmail.com>
2021-11-03 14:15:31 +0900

    refactor: fix spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,8 +14,36 @@ Capybara.server = :webrick
 Capybara.register_driver :headless_chrome do |app|
   Capybara::Selenium::Driver.load_selenium
 
+  if Selenium::WebDriver::VERSION < "4.0.0"
+    setup_selenium_driver_3x
+  else
+    setup_selenium_driver
+  end
+end
+
+def setup_selenium_driver_3x
   caps = Selenium::WebDriver::Remote::Capabilities.chrome(
-    'goog:chromeOptions' => {
+    chromeOptions: {
+      args: %w[headless],
+      # required for Chrome 75+
+      w3c: false,
+    },
+    # For up to Chrome 74
+    loggingPrefs: {
+      browser: "ALL",
+    },
+    # for Chrome 75+
+    "goog:loggingPrefs" => {
+      browser: "ALL",
+    },
+  )
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, desired_capabilities: caps)
+end
+
+def setup_selenium_driver
+  caps = Selenium::WebDriver::Remote::Capabilities.chrome(
+    "goog:chromeOptions" => {
       args: %w[--headless],
     },
     "goog:loggingPrefs" => {
